0cdfe8399b52870b95cd458c334e45abddbaceff
Rashmi Karnik <rashmisag@gmail.com>
2020-06-19 16:49:19 -0700

    Facebook Oauth

--- config.js
+++ config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2999593840108039',
+        clientSecret: '28989c758a3e4843e7450acb574d1b6d'
+    }
 }
